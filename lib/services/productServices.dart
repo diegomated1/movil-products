c0bbcb5cc61d3e456a2c313b8e6ffc73e097a530
diegomated1 <diegocardwr@gmail.com>
2023-02-28 16:01:50 -0500

    first commit

---
+++ lib/services/productServices.dart
@@ -0,0 +1,16 @@
+class ProductApi {
+  String url = 'http://ip.com';
+
+  Future getAll() async {
+    
+  }
+
+  Future addFavorite() async {
+    
+  }
+
+  Future deleteFavorite() async {
+    
+  }
+
+}
---
+++ lib/services/userServices.dart
@@ -0,0 +1,15 @@
+class UserApi {
+  String url = 'http://ip.com';
+
+  Future auth() async {
+    
+  }
+
+  Future login() async {
+    
+  }
+
+  Future register() async {
+    
+  }
+}
