af2f6587ced200daccd1369ceb7ab35bd02f67d1
diegomated1 <diegocardwr@gmail.com>
2023-03-03 18:43:00 -0500

    Register screen form

--- lib/screens/register/register.dart
+++ lib/screens/register/register.dart
@@ -1,11 +1,58 @@
 import 'package:flutter/material.dart';
 
-class Register extends StatelessWidget {
+import '../../widgets/button.dart';
+import '../../widgets/input.dart';
+
+class Register extends StatefulWidget{
   const Register({super.key});
 
   @override
+  State<Register> createState() => _Register();
+}
+
+class _Register extends State<Register> {
+
+  late Map<String, String> userInfo = {
+    "username": '',
+    "email": '',
+    "password": '',
+  };
+
+  change(String label, String newValue){
+    userInfo[label.split(' ').join('_').toLowerCase()] = newValue;
+  }
+
+  var loginForm = GlobalKey<FormState>();
+
+  submit()async{
+    if(loginForm.currentState!.validate()){
+      loginForm.currentState!.save();
+      print(userInfo.toString());
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: Center(
+        child: FittedBox(
+          child: Form(
+            key: loginForm,
+            child: Column(
+              children: [
+                Input(labelText: 'Username', handler: change),
+                Input(labelText: 'Email', handler: change),
+                Input(labelText: 'Password', handler: change, hidden: true,),
+                Button(labelText: 'Register', handler: submit,),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
 }
