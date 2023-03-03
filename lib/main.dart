25343ddaf8c9ba881921bc5d7c329734a23fb6cb
diegomated1 <diegocardwr@gmail.com>
2023-03-03 17:34:02 -0500

    Importing and using get library

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'screens/auth/auth.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,14 +12,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Scaffold(
-        body: Text('Flutter Demo Home Page'),
-      )
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Auth(),
     );
   }
 }
