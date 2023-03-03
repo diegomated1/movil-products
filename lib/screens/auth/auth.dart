f1669dc21182f4c1d6ef1a057c03e865c8a2201e
diegomated1 <diegocardwr@gmail.com>
2023-03-03 17:33:12 -0500

    Importing and using get library

---
+++ lib/screens/auth/auth.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:products/screens/login/login.dart';
+import 'package:products/screens/register/register.dart';
+
+import '../../widgets/button.dart';
+
+class Auth extends StatelessWidget {
+  const Auth({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Auth'),
+      ),
+      body: Center(
+        child: FittedBox(
+          child: Column(
+            children: [
+              Button(
+                labelText: 'Login', 
+                handler: (){
+                  Get.to(const Login());
+                }
+              ),
+              Button(
+                labelText: 'Register', 
+                handler: (){
+                  Get.to(const Register());
+                }
+              ),
+            ],
+          ),
+        )
+      ),
+    );
+  }
+
+}
