7bd4bad6a2df692216dffd8014c9557ebed9377f
diegomated1 <diegocardwr@gmail.com>
2023-03-04 12:37:25 -0500

    Products screen

--- lib/screens/products/productsPage.dart
+++ lib/screens/products/productsPage.dart
@@ -1,11 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:products/models/productModel.dart';
+import 'package:products/screens/products/productListExtended.dart';
+import 'package:products/screens/products/productListSimple.dart';
+import 'package:products/services/productServices.dart';
+import 'package:products/utils/utils.dart';
+import 'package:provider/provider.dart';
 
-class ProductsPage extends StatelessWidget {
+class ProductsPage extends StatefulWidget{
   const ProductsPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  State<StatefulWidget> createState() => _ProductsPage();
+}
+
+class _ProductsPage extends State<ProductsPage> {
+  late Future<ProductsModel?> products;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      products = ProductApi().getAll();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {    
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Products'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(20.0),
+        child: FutureBuilder(
+          future: products,
+          builder: (context, snapshot) {
+            if(snapshot.hasData){
+              return ProductListExtended(products: snapshot.data!.products);
+            }else{
+              return const Center(
+                child: FittedBox(
+                  child: Text('No data'),
+                ),
+              );
+            }
+          },
+        ),
+      ),
+    );
   }
 
 }
