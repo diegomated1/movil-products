5218dbc7aca82300c46cbeb1d5133f2ab9c88f16
diegomated1 <diegocardwr@gmail.com>
2023-03-04 08:51:37 -0500

    remove password field from user model

--- lib/models/userModel.dart
+++ lib/models/userModel.dart
@@ -3,20 +3,17 @@ class UserModel {
     required this.idUser,
     required this.username,
     required this.email,
-    required this.password
   });
   
   final String idUser;
   final String username;
   final String email;
-  final String password;
 
   factory UserModel.fromJson(Map<String, dynamic> json){
     return UserModel(
       idUser: json['id_user'],
       username: json['username'],
       email: json['email'],
-      password: json['password'],
     );
   }
 }
