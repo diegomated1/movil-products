5dd2689faf330400a61f5293ada22e55250478dc
diegomated1 <diegocardwr@gmail.com>
2023-03-04 14:24:27 -0500

    service for get, post and delete favorites

--- lib/services/productServices.dart
+++ lib/services/productServices.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:products/models/favoritesModel.dart';
 import 'package:products/models/productModel.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
@@ -30,12 +31,74 @@ class ProductApi {
     }
   }
 
-  Future addFavorite() async {
-    
+  Future<FavoritesModel> getFavorites() async {
+    try{
+      final prefs = await SharedPreferences.getInstance();
+      var userToken = prefs.getString('userToken');
+      if(userToken==null){
+        return FavoritesModel.fromJson([]);
+      }
+      var headers = {
+        "Content-type": "application/json",
+        "Authorization": "Bearer $userToken"
+      };
+      var response = await http.get(Uri.parse('$url/products/favorite'), headers: headers);
+      if(response.statusCode == 200){
+        var body = json.decode(response.body);
+        return FavoritesModel.fromJson(body['products']);
+      } else {
+        return FavoritesModel.fromJson([]);
+      }
+    }catch(e){
+      print(e.toString());
+        return FavoritesModel.fromJson([]);
+    }
   }
 
-  Future deleteFavorite() async {
-    
+  Future<bool> addFavorite({required String idProduct}) async {
+    try{
+      final prefs = await SharedPreferences.getInstance();
+      var userToken = prefs.getString('userToken');
+      if(userToken==null){
+        return false;
+      }
+      var headers = {
+        "Content-type": "application/json",
+        "Authorization": "Bearer $userToken"
+      };
+      var response = await http.post(Uri.parse('$url/products/favorite/$idProduct'), headers: headers);
+      if(response.statusCode == 200){
+        return true;
+      } else {
+        return false;
+      }
+    }catch(e){
+      print(e.toString());
+        return false;
+    }
+  }
+
+  Future deleteFavorite({required String idProduct}) async {
+    try{
+      final prefs = await SharedPreferences.getInstance();
+      var userToken = prefs.getString('userToken');
+      if(userToken==null){
+        return false;
+      }
+      var headers = {
+        "Content-type": "application/json",
+        "Authorization": "Bearer $userToken"
+      };
+      var response = await http.delete(Uri.parse('$url/products/favorite/$idProduct'), headers: headers);
+      if(response.statusCode == 200){
+        return true;
+      } else {
+        return false;
+      }
+    }catch(e){
+      print(e.toString());
+        return false;
+    }
   }
 
 }
