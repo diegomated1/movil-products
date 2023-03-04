38baa31f507a16c30365fe8823c38abae5b2dd5a
diegomated1 <diegocardwr@gmail.com>
2023-03-04 08:50:55 -0500

    router with get

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
+import 'package:products/providers/userProvider.dart';
+import 'package:products/screens/login/login.dart';
+import 'package:products/screens/products/productsPage.dart';
+import 'package:products/screens/register/register.dart';
+import 'package:provider/provider.dart';
 import 'screens/auth/auth.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +16,34 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Auth(),
+    return ChangeNotifierProvider(
+      create: (context) => UserProvider(),
+      child: GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: '/auth',
+        getPages: [
+          GetPage(
+            name: '/',
+            page: () => const Auth()
+          ),
+          GetPage(
+            name: '/about',
+            page: () => const Login()
+          ),
+          GetPage(
+            name: '/contact',
+            page: () => const Register()
+          ),
+          GetPage(
+            name: '/contact',
+            page: () => const ProductsPage()
+          ),
+        ],
+      )
     );
   }
 }
