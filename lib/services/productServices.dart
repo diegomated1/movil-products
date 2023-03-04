791e4e5e73999bb822e86c17a5963e297c822cdd
diegomated1 <diegocardwr@gmail.com>
2023-03-04 12:34:37 -0500

    getAll products service

--- lib/services/productServices.dart
+++ lib/services/productServices.dart
@@ -1,8 +1,33 @@
-class ProductApi {
-  String url = 'http://ip.com';
+import 'dart:convert';
+import 'package:products/models/productModel.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
-  Future getAll() async {
-    
+class ProductApi {
+  String url = 'http://192.168.0.11:3000';
+
+  Future<ProductsModel> getAll() async {
+    try{
+      final prefs = await SharedPreferences.getInstance();
+      var userToken = prefs.getString('userToken');
+      if(userToken==null){
+        return ProductsModel.fromJson([]);
+      }
+      var headers = {
+        "Content-type": "application/json",
+        "Authorization": "Bearer $userToken"
+      };
+      var response = await http.get(Uri.parse('$url/products'), headers: headers);
+      if(response.statusCode == 200){
+        var body = json.decode(response.body);
+        return ProductsModel.fromJson(body['products']);
+      } else {
+        return ProductsModel.fromJson([]);
+      }
+    }catch(e){
+      print(e.toString());
+      return ProductsModel.fromJson([]);
+    }
   }
 
   Future addFavorite() async {
