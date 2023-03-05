b37dd1f86c867b71ab48b343ba3d695822efa83d
diegomated1 <diegocardwr@gmail.com>
2023-03-04 20:50:58 -0500

    return prodcut extended widget

--- lib/screens/products/productListExtended.dart
+++ lib/screens/products/productListExtended.dart
@@ -1,22 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:products/models/productModel.dart';
 import 'package:products/screens/products/productExtended.dart';
-import 'package:products/screens/products/productSimple.dart';
-import 'package:products/utils/utils.dart';
-
-import '../../services/productServices.dart';
 
 class ProductListExtended extends StatelessWidget{
   const ProductListExtended({
     super.key,
     required this.products,
-    required this.favorites,
-    required this.refresh
   });
 
   final List<ProductModel> products;
-  final Map<String, bool> favorites;
-  final Function() refresh;
 
   @override
   Widget build(BuildContext context) {
@@ -29,38 +21,7 @@ class ProductListExtended extends StatelessWidget{
         mainAxisSpacing: 10,
       ),
       itemBuilder: (context, index) {
-        return Stack(
-          children: [
-            ProductExtended(product: products[index]),
-            Positioned(
-              right: 5,
-              top: 5,
-              child: SizedBox(
-                width: 40,
-                height: 40,
-                child: IconButton(
-                  padding: EdgeInsets.zero,
-                  onPressed: ()async{
-                    bool response;
-                    if(favorites[products[index].idProduct]==true){
-                      response = await ProductApi().deleteFavorite(idProduct: products[index].idProduct);
-                    }else{
-                      response = await ProductApi().addFavorite(idProduct: products[index].idProduct);
-                    }
-                    if(response){
-                      refresh();
-                    }
-                  },
-                  icon: Icon(
-                    Icons.star,
-                    size: 40,
-                    color: (favorites[products[index].idProduct]==true) ? Colors.yellow : Colors.black,
-                  ),
-                ),
-              )
-            )
-          ]
-        );
+        return ProductExtended(product: products[index]);
       },
     );
   }
