720e2b952f0753ecd3d35420fea95a8787bf9070
diegomated1 <diegocardwr@gmail.com>
2023-03-04 20:49:37 -0500

    modify start and add functions to add and delete from favorites

--- lib/screens/products/productExtended.dart
+++ lib/screens/products/productExtended.dart
@@ -5,6 +5,7 @@ import 'package:products/utils/utils.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:products/widgets/button.dart';
 
+import '../../services/favoriteServices.dart';
 import '../../widgets/photo.dart';
 
 class ProductExtended extends StatefulWidget{
@@ -21,42 +22,87 @@ class ProductExtended extends StatefulWidget{
 
 class _ProductExtended extends State<ProductExtended>{
 
+  bool isFavorite = false;
+
+  Future favorite()async{
+    var result = await FavoriteProductsDB.instance.getById(widget.product.idProduct);
+    setState(() {
+      isFavorite = (result.products.isEmpty) ? false : true;
+    });
+  }
+
+  setFavorite()async{
+    if(isFavorite){
+      await FavoriteProductsDB.instance.delete(widget.product.idProduct);
+    }else{
+      await FavoriteProductsDB.instance.insert(widget.product);
+    }
+    setState(() {
+      isFavorite = !isFavorite;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    favorite();
+  }
+
   @override
   Widget build(BuildContext context) {
 
-    //final width = MediaQuery.of(context).size.width;
-
-    return Container(
-      padding: const EdgeInsets.all(5.0),
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.black),
-          borderRadius: const BorderRadius.all(Radius.circular(5)),
-          color: Colors.grey,
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.black45,
-              blurRadius: 10.0,
-            )
-          ]
-        ),
-      child: Column(
-        children: [
-          Expanded(
-            flex: 2,
-            child: Photo(id: widget.product.idProduct,)
-          ),
-          Expanded(
-            flex: 1,
-            child: Column(
-              children: [
-                Info(title: 'Name', child: Text(widget.product.name)),
-                Info(title: 'Seller', child: Text(widget.product.sellerName)),
-                Info(title: 'Calification', child: Text(widget.product.calification.toString())),
-              ],
-            )
+    return Stack(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(5.0),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black),
+              borderRadius: const BorderRadius.all(Radius.circular(5)),
+              color: Colors.grey,
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.black45,
+                  blurRadius: 10.0,
+                )
+              ]
+            ),
+          child: Column(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Photo(id: widget.product.idProduct,)
+              ),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  children: [
+                    Info(title: 'Name', child: Text(widget.product.name)),
+                    Info(title: 'Seller', child: Text(widget.product.sellerName)),
+                    Info(title: 'Calification', child: Text(widget.product.calification.toString())),
+                  ],
+                )
+              )
+            ],
           )
-        ],
-      )
+        ),
+        Positioned(
+          right: 5,
+          top: 5,
+          child: SizedBox(
+            width: 40,
+            height: 40,
+            child: IconButton(
+              padding: EdgeInsets.zero,
+              onPressed: setFavorite,
+              icon: Icon(
+                Icons.star,
+                size: 40,
+                color: (isFavorite) ? Colors.yellow : Colors.black,
+              ),
+            ),
+          )
+        )
+      ]
     );
   }
 }
