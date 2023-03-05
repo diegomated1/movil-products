1638c014503688bc2663257237116a4a9ee2bba4
diegomated1 <diegocardwr@gmail.com>
2023-03-04 20:47:39 -0500

    verify session in auth screen

--- lib/screens/auth/auth.dart
+++ lib/screens/auth/auth.dart
@@ -1,20 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:products/providers/userProvider.dart';
 import 'package:provider/provider.dart';
 import 'package:get/get.dart';
 import 'package:products/screens/login/login.dart';
 import 'package:products/screens/register/register.dart';
 
+import '../../services/favoriteServices.dart';
+import '../../services/userServices.dart';
 import '../../widgets/button.dart';
 
-class Auth extends StatelessWidget {
+class Auth extends StatefulWidget {
   const Auth({super.key});
 
   @override
+  State<StatefulWidget> createState() => _Auth();
+}
+
+class _Auth extends State<Auth> {
+
+  getUser()async{
+    var data = await UserApi().auth();
+    if(data!=null){
+      await FavoriteProductsDB.instance.sync();
+      Get.toNamed('/profile');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getUser();
+  }
+
+  @override
   Widget build(BuildContext context) {
-
-    var usr = context.watch<UserProvider>();
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Auth'),
